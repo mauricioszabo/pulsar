3212eb206b3305c6416c9002c523d2bec42ce41b
Maurício Szabo <mauricio.szabo@gmail.com>
2023-09-22 00:19:32 -0300

    Removed unnecessary function debounce

--- src/text-editor/utils.js
+++ src/text-editor/utils.js
@@ -184,24 +184,6 @@ function constrainRangeToRows(range, startRow, endRow) {
   return range;
 }
 
-// function debounce(fn, wait) {
-//   let timestamp, timeout;
-//
-//   function later() {
-//     const last = Date.now() - timestamp;
-//     if (last < wait && last >= 0) {
-//       timeout = setTimeout(later, wait - last);
-//     } else {
-//       timeout = null;
-//       fn();
-//     }
-//   }
-//
-//   return function() {
-//     timestamp = Date.now();
-//     if (!timeout) timeout = setTimeout(later, wait);
-//   };
-// }
 const NBSP_CHARACTER = '\u00a0';
 
 module.exports = {
